d71febb829b6114bae2d22ac56cec327dfb39783
indirectlylit <devon@indirectlylit.com>
2017-09-14 17:34:34 -0700

    static UI

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,127 @@
 <template>
+
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="top">
+      <img src="./assets/kolibri-logo.svg" width="20%">
+      <h1>Possibly technical terms<br>in the Kolibri ecosystem</h1>
+      <p>Please add any terms that might have a technical definition or meaning.</p>
+      <p>These could be past or present, user-facing or internal.</p>
+    </div>
+
+    <div class="wrapper pure-g">
+      <section class="pure-u-1-2">
+        <h2>Add new term</h2>
+        <form class="pure-form">
+          <div>
+            <input type="text"></input>
+            <button type="submit" class="pure-button">Add</button>
+          </div>
+          <p class="validation">(already added)</p>
+        </form>
+      </section>
+      <section class="pure-u-1-2">
+        <h2>Added terms</h2>
+        <div>
+          <ul>
+            <li>user <a href="#" title="Remove">✖</a></li>
+            <li>learner <a href="#" title="Remove">✖</a></li>
+            <li>admin <a href="#" title="Remove">✖</a></li>
+            <li>coach <a href="#" title="Remove">✖</a></li>
+            <li>device owner <a href="#" title="Remove">✖</a></li>
+            <li>superuser <a href="#" title="Remove">✖</a></li>
+            <li>facility <a href="#" title="Remove">✖</a></li>
+            <li>resource <a href="#" title="Remove">✖</a></li>
+            <li>content <a href="#" title="Remove">✖</a></li>
+            <li>exercise <a href="#" title="Remove">✖</a></li>
+          </ul>
+        </div>
+      </section>
+    </div>
   </div>
+
 </template>
+
 
 <script>
 export default {
   name: 'app',
   data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    return {}
+  let app = Firebase.initializeApp(config)
+  let db = app.database()
+  let booksRef = db.ref('books')
+
+  export default {
+    name: 'app',
+    data () {
+      return {}
     }
   }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin: 30px;
+    text-align: center;
+    margin: auto;
+    max-width: 600px;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  .top {
+    max-width:
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-a {
-  color: #42b983;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  li {
+    position: relative;
+  }
+
+  li a {
+    text-decoration: none;
+    visibility: hidden;
+    padding: 8px;
+    color: #cc3232;
+    position: absolute;
+    right: 0;
+    top: -8px;
+  }
+
+  /*li:hover {
+    background-color: #EEE;
+  }*/
+
+  li:hover a {
+    text-decoration: none;
+    visibility: visible;
+  }
+
+  input {
+  }
+
+  .wrapper {
+  }
+
+  .validation {
+    color: #cc3232;
+    font-size: smaller;
+  }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import purecss from 'purecss'
+import purecssGrids from 'purecss/build/grids-responsive-min.css'
+
 new Vue({
   el: '#app',
   render: h => h(App)
